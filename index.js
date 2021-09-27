2d6e716be21fdf306ac370e888018ecb50c44ebf
kashif naseem <56063644+kashifnaseem50@users.noreply.github.com>
2021-09-28 03:55:50 +0500

    edited

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello Testing DevOps Node! js')
+  response.send('Hello Testing DevOps Node! js AVC')
 }) 
 
 app.listen(app.get('port'), function() {
